79e77354a023f995a33c7823e3aea915b6c51aed
Darek Kaczyński <kaczynskid@gmail.com>
2016-10-26 12:48:02 +0200

    Added CRUd for reservations

--- reservation-service/src/main/java/com/example/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/example/ReservationServiceApplication.java
@@ -1,14 +1,23 @@
 package com.example;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
@@ -29,12 +38,48 @@ public class ReservationServiceApplication {
 @RequestMapping("/reservations")
 class ReservationController {
 
-	@RequestMapping(method = GET)
-	public List<Reservation> listReservations() {
-        log.info("Listing reservations...");
-		return Arrays.stream("Jarek,Piotr,Marek,Mateusz,Maciek,Piotr".split(","))
-			.map(Reservation::new).collect(Collectors.toList());
+    final Map<String, Reservation> reservations = new HashMap<>();
+
+    public ReservationController() {
+        Arrays.stream("Jarek,Piotr,Marek,Mateusz,Maciek".split(","))
+            .map(Reservation::new)
+            .forEach(reservation -> reservations.put(reservation.name, reservation));
+    }
+
+    @RequestMapping(method = GET)
+	public Collection<Reservation> list() {
+        return reservations.values();
 	}
+
+    @RequestMapping(method = POST)
+	public void create(@RequestBody Reservation reservation) {
+        if (reservations.containsKey(reservation.name)) {
+            throw new ReservationAlreadyExists();
+        }
+
+        reservations.put(reservation.name, reservation);
+    }
+
+    @RequestMapping(path = "/{name}", method = GET)
+    public ResponseEntity<?> get(@PathVariable("name") String name) {
+        if (reservations.containsKey(name)) {
+            return ResponseEntity.ok()
+                .header("X-Reservation-Grats", "Well done!")
+                .body(reservations.get(name));
+        }
+        return ResponseEntity.notFound()
+            .header("X-Reservation-Apology", "Sorry man ;)").build();
+    }
+
+    @RequestMapping(path = "/{name}", method = DELETE)
+    @ResponseStatus(NO_CONTENT)
+    public void delete(@PathVariable("name") String name) {
+        reservations.remove(name);
+    }
+}
+
+@ResponseStatus(value = BAD_REQUEST, reason = "Already reserved!")
+class ReservationAlreadyExists extends RuntimeException {
 }
 
 @NoArgsConstructor
