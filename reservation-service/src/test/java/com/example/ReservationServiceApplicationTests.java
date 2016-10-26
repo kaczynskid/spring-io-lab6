657c2a86483b04da3afc6f87f9946b27c7ce5478
Darek Kaczyński <kaczynskid@gmail.com>
2016-10-26 16:55:07 +0200

    Added integration test

--- reservation-service/src/test/java/com/example/ReservationServiceApplicationTests.java
+++ reservation-service/src/test/java/com/example/ReservationServiceApplicationTests.java
@@ -1,16 +1,38 @@
 package com.example;
 
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 public class ReservationServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	@Autowired
+	WebApplicationContext webApplicationContext;
+
+	MockMvc mvc;
+
+	@Before
+	public void setUp() {
+		mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 	}
 
+	@Test
+	public void listsReservations() throws Exception {
+		mvc.perform(get("/reservations/1"))
+			.andDo(print())
+			.andExpect(jsonPath("@.name").value("Jarek"));
+	}
 }
