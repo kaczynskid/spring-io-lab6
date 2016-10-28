8b92473c01cd1d3c15b3fb44dafee0ef459ff340
Darek Kaczyński <kaczynskid@gmail.com>
2016-10-28 12:59:40 +0200

    Added curcuit breaker

--- reservation-client/src/main/java/com/example/ReservationClientApplication.java
+++ reservation-client/src/main/java/com/example/ReservationClientApplication.java
@@ -1,10 +1,11 @@
 package com.example;
 
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -12,6 +13,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -29,6 +31,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -37,6 +40,7 @@ import lombok.extern.slf4j.Slf4j;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class ReservationClientApplication {
 
 	public static void main(String[] args) {
@@ -46,6 +50,32 @@ public class ReservationClientApplication {
     @Bean @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+}
+
+@Slf4j
+@Component
+class ReservationsService {
+
+    private final ReservationsClient client;
+
+    public ReservationsService(ReservationsClient client) {
+        this.client = client;
+    }
+
+    @HystrixCommand(fallbackMethod = "listReservationsFallback")
+    public Resources<Reservation> listReservationsSafely() {
+        log.info("Calling listReservationsSafely...");
+        return client.listReservations();
+    }
+
+    public Resources<Reservation> listReservationsFallback() {
+        log.info("Calling listReservationsFallback...");
+        return new Resources(
+            asList("This is fallback".split(" ")).stream()
+                .map(Reservation::new)
+                .collect(toList())
+        );
     }
 }
 
@@ -63,12 +93,15 @@ class ReservationsController {
 
     private final DiscoveryClient discovery;
     private final RestTemplate rest;
-    private final ReservationsClient feignClient;
+    private final ReservationsClient client;
+    private final ReservationsService service;
 
-    public ReservationsController(DiscoveryClient discovery, RestTemplate rest, ReservationsClient feignClient) {
+    public ReservationsController(DiscoveryClient discovery, RestTemplate rest,
+                                  ReservationsClient client, ReservationsService service) {
         this.discovery = discovery;
         this.rest = rest;
-        this.feignClient = feignClient;
+        this.client = client;
+        this.service = service;
     }
 
     @RequestMapping(path = "/byName/{name}", method = GET)
@@ -101,15 +134,23 @@ class ReservationsController {
             rest.exchange("http://reservationservice/reservations", HttpMethod.GET, null, responseType);
         return exchange.getBody().getContent().stream()
             .map(Reservation::getName)
-            .collect(Collectors.toList());
+            .collect(toList());
     }
 
     @RequestMapping(path = "/feign-names", method = GET)
     public List<String> feignNames() {
         log.info("Calling feign-names...");
-        return feignClient.listReservations().getContent().stream()
+        return client.listReservations().getContent().stream()
             .map(Reservation::getName)
-            .collect(Collectors.toList());
+            .collect(toList());
+    }
+
+    @RequestMapping(path = "/hystrix-names", method = GET)
+    public List<String> hystrixNames() {
+        log.info("Calling hystrix-names...");
+        return service.listReservationsSafely().getContent().stream()
+            .map(Reservation::getName)
+            .collect(toList());
     }
 }
 
