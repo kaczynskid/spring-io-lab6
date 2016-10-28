ea838ebbee53ef00fc6fee1a506f123e30a2f2ab
Darek Kaczyński <kaczynskid@gmail.com>
2016-10-28 09:25:30 +0200

    Added DiscoveryClient example

--- reservation-client/src/main/java/com/example/ReservationClientApplication.java
+++ reservation-client/src/main/java/com/example/ReservationClientApplication.java
@@ -1,8 +1,15 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,5 +17,36 @@ public class ReservationClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationClientApplication.class, args);
+	}
+}
+
+@Slf4j
+@Component
+class DiscoveryClientExample implements ApplicationRunner {
+
+	private final DiscoveryClient discoveryClient;
+
+	@Autowired
+	public DiscoveryClientExample(DiscoveryClient discoveryClient) {
+		this.discoveryClient = discoveryClient;
+	}
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+		try {
+			log.info("------------------------------");
+			log.info("DiscoveryClient Example");
+
+			discoveryClient.getInstances("reservationservice").forEach(instance -> {
+				log.info("Reservation service: ");
+				log.info("  ID: {}", instance.getServiceId());
+				log.info("  URI: {}", instance.getUri());
+				log.info("  Meta: {}", instance.getMetadata());
+			});
+
+			log.info("------------------------------");
+		} catch (Exception e) {
+			log.error("DiscoveryClient Example Error!", e);
+		}
 	}
 }
